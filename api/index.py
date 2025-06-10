a33b0f31b67cb9273913f001864f8086bc9b87d4
Future Team <shayanwebgame@gmail.com>
2025-06-10 11:11:09 +0500

    remove all filterations in get all salons

--- api/index.py
+++ api/index.py
@@ -55,20 +55,8 @@ def is_valid_pakistani_phone_number(phone):
 def getAllSalon():
     ref = db.reference("salons")
     result = ref.get() or []
-    # Create res_bookings with all fields except 'code' and 'id'
-    # res_salons = [
-    #     {key: value for key, value in salon.items() if key not in ["password"]}
-    #     for salon in result
-    # ]
-    # Convert to list if result is a dictionary
-    salons = list(result.values()) if isinstance(result, dict) else result
-    # Filter out DeActive salons and exclude password field
-    res_salons = [
-        {key: value for key, value in salon.items() if key not in ["password"]}
-        for salon in salons
-        if isinstance(salon, dict) and salon.get("status") == "Active"
-    ]
-    return jsonify({"status": "success", "data": res_salons})
+
+    return jsonify({"status": "success", "data": result})
 @app.route("/get_your_salon", methods=["POST", "OPTIONS"])
 def get_your_salon():
     if request.method == "OPTIONS":
