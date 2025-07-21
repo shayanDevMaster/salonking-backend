b2a1b51f6c71790d524ab01377588ba205b55a93
Future Team <shayanwebgame@gmail.com>
2025-07-21 13:13:01 +0500

    fixing some thing

--- api/index.py
+++ api/index.py
@@ -424,12 +424,12 @@ def get_LoginStudentData():
             pur = random.randint(0, purpose.__len__() - 1)
             delayDays = (pur * 30) + 10
             due_date = (datetime(datetime.today().year, 1, 1) + timedelta(days=delayDays)).strftime("%Y-%m-%d")
-            pay_date = (datetime(datetime.today().year, 1, 1) + timedelta(days=delayDays) + timedelta(days=random.randint(-10 , 20))).strftime("%Y-%m-%d") if random.choice([True, False]) else None
+            pay_date = (datetime(datetime.today().year, 1, 1) + timedelta(days=delayDays + random.randint(-10 , 20))).strftime("%Y-%m-%d") if random.choice([True, False]) else None
             fee = FeeHistory(
                 record_Id=str(random.randint(100, 1000)),
                 class_number=str(random.choice([5])),
                 dueDate=due_date,
-                payDate=pay_date
+                payDate=pay_date,
                 purpose=purposes[pur],
                 amount=random.choice([1500, 2000, 2500]),
                 status="Paid" if pay_date else "Unpaid"
